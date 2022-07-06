98c17b54b72e0b0fe6c956f9ac2dff5e316b71c5
CARLOS ENRIQUE HERNANDEZ GUERRA <cehernandez@unicauca.edu.co>
2022-07-06 13:57:50 -0500

    ACT BOTON SUC

--- CHernandez-JBonilla/Form1.Designer.cs
+++ CHernandez-JBonilla/Form1.Designer.cs
@@ -49,6 +49,7 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.tcGestionBanco.SuspendLayout();
             this.tpRegBanco.SuspendLayout();
             this.tpRegSuc.SuspendLayout();
@@ -142,6 +143,7 @@
             // 
             // tpRegSuc
             // 
+            this.tpRegSuc.Controls.Add(this.button1);
             this.tpRegSuc.Controls.Add(this.comboBox1);
             this.tpRegSuc.Controls.Add(this.textBox4);
             this.tpRegSuc.Controls.Add(this.textBox3);
@@ -251,6 +253,15 @@
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 9;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 46);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "REGISTRAR SUCURSAL";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -291,6 +302,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
     }
 }
 
